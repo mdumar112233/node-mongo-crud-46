e18dcd547aa12db1bbfe3b6f28f97e8a40c09a13
mdumar112233 <mdumarfaruk187326@gmail.com>
2021-03-25 20:36:14 +0600

    working done crud oparetion

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
 const uri = "mongodb+srv://organicdb:gJlvwY7mzcO0DWGS@cluster0.ij0ac.mongodb.net/organicdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -19,13 +20,22 @@ app.get('/', (req, res) => {
 client.connect(err => {
   const collection = client.db("organicdb").collection("products");
 
+  // get method (data come from database)
   app.get('/products', (req, res) =>{
-    collection.find({}).limit(2)
+    collection.find({}).limit(3)
     .toArray((err, documents) => {
       res.send(documents);
     })
   })
 
+  app.get('/product/:id', (req, res) => {
+    collection.find({_id: ObjectId(req.params.id)})
+    .toArray((err, documents) => {
+      res.send(documents[0])
+    })
+  })
+
+  // post method (data set to database)
   app.post('/addProduct', (req, res) => {
     const product = req.body;
     console.log(product);
@@ -35,7 +45,28 @@ client.connect(err => {
       res.send('success')
     })
   })
+
+  // update data with patch(database data update)
+  app.patch('/update/:id', (req, res) => {
+    collection.updateOne({_id: ObjectId(req.params.id)},{
+      $set: {price: req.body.price, quantity: req.body.quantity}
+    })
+    .then(res => {
+      console.log('success');
+    })
+  })
+
+
+// delete method (data delete from database)
+app.delete('/delete/:id', (req, res) => {
+  collection.deleteOne({_id: ObjectId(req.params.id)})
+  .then((result) => {
+    console.log(result);
+  })
+})
 });
+
+
 
 
 app.listen(3000);
