37947d990a064b46a870b0ebdceed12d0a28602a
mdumar112233 <mdumarfaruk187326@gmail.com>
2021-03-25 16:45:39 +0600

    loaded data from our own api

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const express = require('express');
-const password = 'gJlvwY7mzcO0DWGS';
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
-const uri = "mongodb+srv://organicdb:gJlvwY7mzcO0DWGS@cluster0.ij0ac.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://organicdb:gJlvwY7mzcO0DWGS@cluster0.ij0ac.mongodb.net/organicdb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.sendFile(__dirname + '/index.html');
 })
 
 
 
-const MongoClient = require('mongodb').MongoClient;
-
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+  const collection = client.db("organicdb").collection("products");
+
+  app.get('/products', (req, res) =>{
+    collection.find({}).limit(2)
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
+
+  app.post('/addProduct', (req, res) => {
+    const product = req.body;
+    console.log(product);
+    collection.insertOne(product)
+    .then(result => {
+      console.log('one product added');
+      res.send('success')
+    })
+  })
 });
 
 
